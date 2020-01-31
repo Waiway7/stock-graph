c4f060d2e007ae88a29f6757b663195e4a0db35e
ugp <wcc325@nyu.edu>
2020-01-30 20:58:13 -0500

    install intrinio sdk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import "./styles/index.scss";
 // import yodaStitch from "./images/yoda-stitch.jpg";
 // require('dotenv').config();
+
+const apiKey = process.env.API_KEY
+
+var intrinioSDK = require('intrinio-sdk');
+intrinioSDK.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = `${apiKey}`;
+
+var stockExchangeAPI = new intrinioSDK.StockExchangeApi();
+
+var identifier = "USCOMP"; // String | A Stock Exchange identifier (MIC or Intrinio ID)
+
+
+stockExchangeAPI.getStockExchangeById(identifier).then(function(data) {
+  console.log(data);
+}, function(error) {
+  console.error(error);
+});
 const testObj = {
   key1: "hi",
   key2: {
