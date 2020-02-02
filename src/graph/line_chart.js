41a07666a728e85c3733c14803e3470a7460f283
ugp <wcc325@nyu.edu>
2020-02-02 14:25:35 -0500

    start of css on line graph x axis

--- src/graph/line_chart.js
+++ src/graph/line_chart.js
@@ -4,38 +4,48 @@ const parseData = (data) => {
     const dataArr = [];
     for (let i = data.length - 1; i >= 0; i--){
         dataArr.push({
-            "date": data[i].date,
+            "date": new Date(data[i].date),
             "close": data[i].close
         })
     }
     console.log(dataArr)
     return dataArr
 }
+
+
 export function drawChart(dataArr){
+    const parentDiv = document.getElementById("chart");
+    const svgWidth = 600
+    const svgHeight = 400
     const data = parseData(dataArr);
-    const svgWidth = 1500, svgHeight = 400;
+    // const svgWidth = 1500, svgHeight = 400;
     const margin = { top: 20, right: 20, bottom: 30, left: 50};
     const width = svgWidth - margin.left - margin.right;
     const height = svgHeight - margin.top - margin.bottom;
-    const svg = d3.select("#chart").append("svg")
+    const svg = d3.select("#chart")
+                    .append("div")
+                    .classed("svg-container", true)
+                    .append("svg")
+                    .attr("preserveAspectRatio", "xMinYMin meet")
+                    .attr("viewBox", "0 0 600 400")
+                    .classed("svg-content-responsive", true)
+                    .append("svg")
                     .attr("width", svgWidth)
                     .attr("height", svgHeight)
                     .style("border", "1px solid lightgray")
     const g = svg.append("g")
                 .attr("transform", "translate(" + margin.left + ", "+ margin.top + ")")
-    const x = d3.scaleTime().rangeRound([0, width]);
+    const x = d3.scaleTime().rangeRound([0, width]).nice();
     const y = d3.scaleLinear().rangeRound([height, 0])
 
     const line = d3.line()
                     .x(function(d){return x(d.date)})
                     .y(function(d){return y(d.close)})
-                    
-    x.domain(d3.extent(data, function(d){return d.date}));
+
+    x.domain(d3.extent(data, function(d){return d.date})).nice();;
     y.domain(d3.extent(data, function(d){return d.close}));
     g.append("g")
         .attr("transform", "translate(0, " + height + ")")
         .call(d3.axisBottom(x))
-       
-
 }
 
