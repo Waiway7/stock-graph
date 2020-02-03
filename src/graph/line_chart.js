13065113082c83ff4623b089a51419305f0cb427
ugp <wcc325@nyu.edu>
2020-02-02 22:06:30 -0500

    finished plotting line graph

--- src/graph/line_chart.js
+++ src/graph/line_chart.js
@@ -14,24 +14,35 @@ const parseData = (data) => {
 
 export function drawChart(dataArr){
     const parentDiv = document.getElementById("chart");
-    const svgWidth = 600
-    const svgHeight = 400
+    // const svgWidth = 600
+    // const svgHeight = 400
     const data = parseData(dataArr);
     // const svgWidth = 1500, svgHeight = 400;
-    const margin = { top: 20, right: 20, bottom: 30, left: 50};
-    const width = svgWidth - margin.left - margin.right;
-    const height = svgHeight - margin.top - margin.bottom;
-    const svg = d3.select("#chart")
+    
+    const div = d3.select("#chart")
                     .append("div")
                     .classed("svg-container", true)
-                    .append("svg")
+
+    const svg = div.append("svg")
+                    .classed("svg-graph", true)
                     .attr("preserveAspectRatio", "xMinYMin meet")
-                    .attr("viewBox", "0 0 700 400")
-                    // .classed("svg-content-responsive", true)
-                    // .append("svg")
-                    .attr("width", "70%")
-                    .attr("height", "70%")
-                    .style("border", "1px solid lightgray")
+                    .attr("viewBox", "0 0 850 400")
+    
+    const findSVG = document.querySelector(".svg-graph")
+    const svgWidth = 800;
+    const svgHeight = 400;
+
+
+    const infoContainer = d3.select(".svg-container")
+                            .append("div")
+                            .classed("security-info", true)
+   
+
+    const margin = { top: 20, right: 20, bottom: 30, left: 18};
+    const width = svgWidth - margin.left - margin.right;
+    // console.log(width)
+    const height = svgHeight - margin.top - margin.bottom;
+
     const g = svg.append("g")
                 .attr("transform", "translate(" + margin.left + ", "+ margin.top + ")")
     const x = d3.scaleTime().range([0, width]).nice();
@@ -46,12 +57,13 @@ export function drawChart(dataArr){
         .nice();
     y.domain(d3.extent(data, function(d){return d.close}))
         .nice();
+
     g.append("g")
         .attr("transform", "translate(0, " + height + ")")
         .call(d3.axisBottom(x)
             .tickFormat(function(d){
-                const month = d.getMonth() + 1
-                const day = d.getDate();
+                const month = d.getMonth().toString().length === 1 ? `0${d.getMonth() + 1}` : d.getMonth();
+                const day = d.getDate().toString().length === 1 ? "0" + d.getDate().toString() : d.getDate();
                 if (month === 1 && day === 1){
                     return d.getFullYear()
                 }
@@ -60,6 +72,16 @@ export function drawChart(dataArr){
         )
 
     g.append("g")
+        .attr("transform", "translate(" + width +", "+ 0 + ")")
         .call(d3.axisRight(y));
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line)
 }
 
