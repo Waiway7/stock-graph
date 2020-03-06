3cafd9b75cb95e0c07b8f80a13d3cd2769b2e0d2
ugp <wcc325@nyu.edu>
2020-03-05 22:04:06 -0500

    testing env file for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,19 +4,4 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-             'API_KEY': JSON.stringify(process.env.API_KEY)
-          }
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
-            filename: "[name].css",
-            chunkFilename: "[id].css",
-            ignoreOrder: false // Enable to remove warnings about conflicting order
-          }),
-          require("autoprefixer")
-        ],
 });
