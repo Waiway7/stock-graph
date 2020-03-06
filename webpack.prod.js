505d059a986336774498eae8ee9091cb20fefc3b
ugp <wcc325@nyu.edu>
2020-03-05 21:57:42 -0500

    testing env file for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,12 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+             'API_KEY': JSON.stringify(process.env.API_KEY)
+          }
+        })
+      ]
 });
