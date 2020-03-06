35f9bac91d55e43959e3bad70679e49600e1113a
ugp <wcc325@nyu.edu>
2020-03-05 22:01:09 -0500

    testing env file for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,6 +9,14 @@ module.exports = merge(common, {
           'process.env': {
              'API_KEY': JSON.stringify(process.env.API_KEY)
           }
-        })
-      ]
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+            ignoreOrder: false // Enable to remove warnings about conflicting order
+          }),
+          require("autoprefixer")
+        ],
 });
